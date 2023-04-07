9d19c1e346f81bdfd8504c9954eb922a4061d44b
Javez <mr.javez@ukr.net>
2023-04-08 02:03:09 +0300

    Change: -sidebar animation -question animation

--- login-page/resources/js/question.js
+++ login-page/resources/js/question.js
@@ -2,9 +2,15 @@ let activeQuestion = false;
 
 const openCloseQuestion = () => {
     if(activeQuestion === false) {
+       
+        document.getElementById("help-span").style.opacity = "1";
+        document.getElementById("help-span").style.transition = "visibility 2s linear 2s, opacity 2s;"
         document.getElementById("help-span").style.visibility = "visible";
         activeQuestion = true;
     } else {
+        
+        document.getElementById("help-span").style.opacity = "0";
+        document.getElementById("help-span").style.transition = "visibility 2s linear 2s, opacity 2s"
         document.getElementById("help-span").style.visibility = "hidden";
         activeQuestion = false;
     }
